2c1e1cdcc5307ad13ce689b4394cb934b573ccc7
Annan Deriba <annandejene2020@gmail.com>
2024-09-03 18:35:58 +0300

    Changed position of text in hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,10 +3,7 @@ import hero from "../assets/hero.jpg";
 
 const Hero = () => {
   return (
-    <section
-      id=""
-      className="relative h-screen flex justify-center items-center"
-    >
+    <section className="relative h-screen flex justify-center items-center">
       <div className="absolute inset-0 -z-20 h-full w-full overflow-hidden">
         <video
           src={video}
@@ -21,7 +18,7 @@ const Hero = () => {
 
       <div className="absolute inset-0 -z-10 bg-gradient-to-b from-transparent to-black" />
 
-      <div className="relative z-20 flex flex-col p-4 gap-5">
+      <div className="relative z-20 flex self-end flex-col px-4 py-36 gap-5">
         <h1 className="text-6xl lg:text-7xl font-semibold hero-heading">
           Where Flavors Come Together
         </h1>
