19849cb18fec76b94499b28b7271207ce3933774
Annan Deriba <annandejene2020@gmail.com>
2024-09-04 12:01:13 +0300

    Finished the about page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Dishes from "./components/Dishes";
+import About from "./components/About";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Navbar />
       <Hero />
       <Dishes />
+      <About />
     </main>
   );
 }
---
+++ src/components/About.jsx
@@ -0,0 +1,30 @@
+import about from "../assets/about.jpg";
+import { ABOUT } from "../constants";
+
+const About = () => {
+  return (
+    <section id="about" className="container mx-auto h-screen py-16">
+      <h2 className="mb-16 text-center text-3xl lg:text-4xl">About Us</h2>
+
+      <div className="flex flex-wrap">
+        <div className="w-full p-4 lg:w-1/2">
+          <img
+            src={about}
+            alt="about"
+            className="mx-auto h-[680px] rounded-3xl lg:-rotate-3"
+          />
+        </div>
+
+        <div className="w-full px-2 text-center lg:w-1/2 lg:text-left">
+          <h3 className="text-4xl md:text-5xl">{ABOUT.header}</h3>
+          <div className="mt-1 h-1 w-36 -rotate-3 bg-rose-300" />
+          <p className="m-8 text-2xl leading-normal tracking-tight text-[#a7a3a3] lg:max-w-[480px] lg:leading-relaxed">
+            {ABOUT.content}
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
